efed7c03a327418ca23677afaddfba1c6da99531
Felix Sager <felix.sager@alumni.fh-aachen.de>
2020-05-17 02:10:51 +0200

    install ohne errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 const path = require('path');
+
 module.exports = {
+  mode: "development",
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: "/assets/"
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        query: {
-          presets: [ 'es2015', 'react']
+        options: {
+          presets: [ '@babel/preset-env', '@babel/preset-react']
         }
       }
     ]
